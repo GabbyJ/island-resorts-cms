b2aeca6c0204cea7c40760027428f954d1b495af
gabrielle <gabrielle@Gabrielles-MacBook-Pro-2.local>
2020-09-09 15:52:47 -0500

    Add filters for Guests, Price, Size, Extras

--- src/Components/RoomFilter.jsx
+++ src/Components/RoomFilter.jsx
@@ -14,6 +14,7 @@ export default function RoomFilter({rooms}){
         handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets
     } = context;
 
+    //Room Type
     // get unique types
     let types = getUnique(rooms, "type");
     //add all
@@ -22,19 +23,66 @@ export default function RoomFilter({rooms}){
     types = types.map((item, index) => {
         return <option value={item} key={index}>{item}</option>
     })
+    //end Room Type
+
+    //Guests
+    let people = getUnique(rooms, "capacity");
+    people = people.map((item, index) =>{
+        return <option key={index} value={item}>{item}</option>
+    })
+    //end Guests
 
     return(
         <section className="filter-container">
             <Title title="search rooms"/>
             <form className="filter-form">
-                {/* select type */}
+                {/* Room Type */}
                 <div className="form-group">
                     <label htmlFor="type">room type</label>
                     <select name="type" id ="type" value={type} className="form-control" onChange={handleChange}>
                         {types}
                     </select>
                 </div>
-                {/* end of select type */}
+                {/* end of Room Type */}
+                {/* Guests */}
+                <div className="form-group">
+                    <label htmlFor="capacity">guests</label>
+                    <select name="capacity" id ="capacity" value={capacity} className="form-control" onChange={handleChange}>
+                        {people}
+                    </select>
+                </div>
+                {/* end of Guests */}
+                {/* Price */}
+                <div className="form-group">
+                    <label htmlFor = "price">
+                        room price ${price}
+                    </label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control"/>
+                </div>
+                {/* end Price */}
+                {/* Size */}
+                <div className="form-group">
+                    <label htmlFor="size">room size</label>
+                    <div className ="size-inputs">
+                        {/* <p>Min: </p> */}
+                        <input type="number" name="minSize" id="size" value={minSize} onChange={handleChange} className="size-input"/>
+                        {/* <p>Max: </p> */}
+                        <input type="number" name="maxSize" id="size" value={maxSize} onChange={handleChange} className="size-input"/>
+                    </div>
+                </div>
+                {/* end Size */}
+                {/* Extras */}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input type="checkbox" name="breakfast" id="breakfast" checked={breakfast} onChange={handleChange}/>
+                        <label htmlFor="breakfast">breakfast</label>
+                    </div>
+                    <div className="single-extra">
+                        <input type="checkbox" name="pets" id="pets" checked={pets} onChange={handleChange}/>
+                        <label htmlFor="pets">pets</label>
+                    </div>
+                </div>
+                {/* end Extras */}
             </form>
         </section>
     )
--- src/Context.jsx
+++ src/Context.jsx
@@ -59,7 +59,7 @@ class RoomProvider extends Component{
     handleChange = event => {
         // Since checkbox is needed, we use this code
         const target = event.target
-        const value = event.type === "checkbox" ? target.checked : target.value
+        const value = target.type === "checkbox" ? target.checked : target.value
         const name = event.target.name;
         this.setState({
             [name]:value
@@ -77,11 +77,40 @@ class RoomProvider extends Component{
             rooms, type, capacity, price, minSize, maxSize, breakfast, pets
         } = this.state
 
+        //all the rooms
         let tempRooms = [...rooms];
+
+        //transform value
+        capacity = parseInt(capacity)
+        price = parseInt(price)
+
+        //filter by room type
         if (type !== "all"){
             tempRooms = tempRooms.filter(room => room.type === type)
         }
 
+        //filter by capacity
+        if(capacity !== 1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        //filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price)
+
+        //filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+        //filter by breakfast
+        if (breakfast){
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+
+        //filter by pets
+        if (pets){
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
+
+        //change state
         this.setState({
             sortedRooms:tempRooms
         })
