4b13725f6d46597abf5a0f65a83a5c8f9c19cabd
gabrielle <gabrielle@Gabrielles-MacBook-Pro-2.local>
2020-09-10 15:45:09 -0500

    Made API tokens private

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from "contentful";
 
 export default createClient({
-    space: "hl4es7g922wz",
-    accessToken: "Errh7cG-wWZQqoCVC_X2Xq5jUArSd2g5vl-zZ7QdtX8"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
